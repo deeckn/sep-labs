71efc489e86206c8083e2200ecdb9d8b53d6b85f
Zorionn <90766449+Zorionn@users.noreply.github.com>
2022-02-24 16:57:04 +0700

    Update simple_drawing_window3.py

--- Lab8/Q1/simple_drawing_window3.py
+++ Lab8/Q1/simple_drawing_window3.py
@@ -1,4 +1,3 @@
-import sys
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
@@ -14,37 +13,20 @@ class SimpleDrawingWindow3(QWidget):
         p = QPainter()
         p.begin(self)
 
-        p.setPen(QColor(169,125,100))
-        p.setBrush(QColor(169,125,100))
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
         p.drawPolygon([
-            QPoint(70, 100), QPoint(130, 100),
-            QPoint(130, 150), QPoint(70, 150),
+            QPoint(70, 100), QPoint(100, 110),
+            QPoint(130, 100), QPoint(100, 150),
         ])
 
-        p.setPen(QColor(14,174,174))
-        p.setBrush(QColor(14,174,174))
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
 
         p.drawPolygon(
-            [QPoint(50, 150), QPoint(150, 150), QPoint(150, 300), QPoint(50, 300),]
-        )
-
-        p.setPen(QColor(73,70,151))
-        p.setBrush(QColor(73,70,151))
-
-        p.drawPolygon(
-            [QPoint(50, 300), QPoint(150, 300), QPoint(150, 400), QPoint(50, 400),]
+            [QPoint(50, 200), QPoint(150, 200), QPoint(100, 400), ]
         )
 
         p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
-
-def main():
-    app = QApplication(sys.argv)
- 
-    w = SimpleDrawingWindow3()
-    w.show()
-
-    return app.exec_()
-
-if __name__ == "__main__":
-    sys.exit(main())
