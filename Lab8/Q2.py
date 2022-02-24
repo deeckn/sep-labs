5ffcecdf03c5560bc7b04259f7559f89e71838cc
Dee Deesit <40157947+deeckn@users.noreply.github.com>
2022-02-24 17:34:04 +0700

    Add import statement

---
+++ Lab8/Q2.py
@@ -0,0 +1,10 @@
+import sys
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+
+# Paint Window - Mark - Delete this
+
+# SPP - Fay - Delete this
+
+# Main - Kris - Delete this
